0444ce98c491aa926fd72fe12b69cb4fd06a52fa
Luis <luisdoin@gmail.com>
2021-09-14 11:38:15 -0300

    Creating DbSession

---
+++ BankRestApi/Data/DbSession.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BankRestApi.Data
+{
+    public sealed class DbSession : IDisposable
+    {
+        public IDbConnection Connection { get; }
+        public IDbTransaction Transaction { get; set; }
+
+        public DbSession()
+        {
+            Connection = new SqlConnection(Settings.ConnectionString);
+            Connection.Open();
+        }
+
+        public void Dispose() => Connection?.Dispose();
+    }
+}
