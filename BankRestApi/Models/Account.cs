f34d4969d621a54462da1b459819c1bec280a8e7
Luis <luisdoin@gmail.com>
2021-09-14 11:51:01 -0300

    Creating models

---
+++ BankRestApi/Models/Account.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankRestApi.Models
+{
+    public class Account
+    {
+        public String AccountNumber { get; set; }
+
+        public double Balance { get; set; }
+    }
+}
---
+++ BankRestApi/Models/Statement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankRestApi.Models
+{
+    public class Statement
+    {
+        public DateTime Date { get; set; }
+
+        public String Description { get; set; }
+
+        public double BalanceVariation { get; set; }
+    }
+}
